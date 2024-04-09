1f9f4d77b87f34e18a7896845085d5f097723486
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-04-08 23:11:47 -0300

    cambios 08/04/2024 23:11 v.01

---
+++ src/inicio/inicio.js
@@ -0,0 +1,8 @@
+ // Seleccionar el elemento con la clase 'logo'
+ var logo = document.getElementById('logo');
+
+ // Agregar un event listener para el evento 'click'
+ logo.addEventListener('click', function() {
+     // Redireccionar a la página de inicio
+     window.location.href = "/index.html";
+ });
