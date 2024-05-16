ae869853c5019d67a88fdc3d98403f9c7b0a412d
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-05-16 14:20:44 -0300

    fix inicio navegadores v01

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -8,29 +8,29 @@
  });
 
 
-// Esperar a que el documento se haya cargado completamente
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtener todos los botones "View More"
-    var viewMoreButtons = document.querySelectorAll(".overlay button");
+// // Esperar a que el documento se haya cargado completamente
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Obtener todos los botones "View More"
+//     var viewMoreButtons = document.querySelectorAll(".overlay button");
 
-    // Iterar sobre cada botón
-    viewMoreButtons.forEach(function(button) {
-        // Agregar un evento de clic a cada botón
-        button.addEventListener("click", function() {
-            // Obtener el enlace asociado al botón y redirigir al usuario
-            var link = button.parentElement.querySelector("a");
-            if (link) {
-                window.location.href = link.href;
-            }
-        });
-    });
+//     // Iterar sobre cada botón
+//     viewMoreButtons.forEach(function(button) {
+//         // Agregar un evento de clic a cada botón
+//         button.addEventListener("click", function() {
+//             // Obtener el enlace asociado al botón y redirigir al usuario
+//             var link = button.parentElement.querySelector("a");
+//             if (link) {
+//                 window.location.href = link.href;
+//             }
+//         });
+//     });
 
-    // Ajustar los estilos de los títulos de los proyectos para asegurar la visibilidad
-    var projectTitles = document.querySelectorAll(".overlay h2");
-    projectTitles.forEach(function(title) {
-        // Ajustar el color del texto y otros estilos según sea necesario
-        title.style.color = "#ffffff"; // Color blanco para el texto
-        title.style.fontSize = "1.2rem"; // Tamaño de fuente adecuado
-        // Otros ajustes de estilo aquí...
-    });
-});
+//     // Ajustar los estilos de los títulos de los proyectos para asegurar la visibilidad
+//     var projectTitles = document.querySelectorAll(".overlay h2");
+//     projectTitles.forEach(function(title) {
+//         // Ajustar el color del texto y otros estilos según sea necesario
+//         title.style.color = "#ffffff"; // Color blanco para el texto
+//         title.style.fontSize = "1.2rem"; // Tamaño de fuente adecuado
+//         // Otros ajustes de estilo aquí...
+//     });
+// });
