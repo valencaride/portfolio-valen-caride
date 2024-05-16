a198de734cede5a2cd15c38540b71a0dd33f5fb8
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-05-16 15:15:40 -0300

    fix inicio navegadores v01

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -7,40 +7,5 @@
      window.location.href = "/index.html";
  });
 
-        // Función para manejar eventos táctiles y de clic en los botones "View More"
-        function handleButtonClick(event) {
-            // Evitar el comportamiento predeterminado del evento (navegación)
-            event.preventDefault();
-            // Obtener el enlace asociado al botón y redirigir al usuario
-            var link = this.parentElement.querySelector("a");
-            if (link) {
-                window.location.href = link.href;
-            }
-        }
-
-        // Función para mostrar los títulos de los proyectos y los botones "View More" al tocar la card
-        function handleCardTouch(event) {
-            // Mostrar el título del proyecto y el botón "View More"
-            var overlay = this.querySelector(".overlay");
-            if (overlay) {
-                overlay.style.opacity = "1";
-            }
-        }
-
-        // Esperar a que el documento se haya cargado completamente
-        document.addEventListener("DOMContentLoaded", function() {
-            // Obtener todas las cards
-            var cards = document.querySelectorAll(".item");
-            // Iterar sobre cada card
-            cards.forEach(function(card) {
-                // Agregar un evento táctil para mostrar el título y el botón
-                card.addEventListener("touchstart", handleCardTouch);
-                // Obtener el botón "View More" dentro de la card
-                var button = card.querySelector(".overlay button");
-                if (button) {
-                    // Agregar un evento de clic para manejar el botón "View More"
-                    button.addEventListener("click", handleButtonClick);
-                }
-            });
-        });
+        
     
