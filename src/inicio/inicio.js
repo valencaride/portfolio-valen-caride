d3ff33318c0c613d2a786bd42c9c370b395923b8
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-05-31 16:14:44 -0300

    fix inicio v05

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -1,37 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.card');
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     cards.forEach(card => {
-        if (isMobile) {
-            card.dataset.clickCount = 0; // Inicializa el contador de clics
-
-            card.addEventListener('click', function(event) {
-                toggleOverlay(this, event);
-            });
-        } else {
-            card.addEventListener('click', function(event) {
-                const url = 'https://www.valencaride.com/src/cards/card-1/index.html';
-                if (url) {
-                    window.location.href = url; // Redirigir a la URL en escritorio
-                }
-            });
-        }
+        card.addEventListener('click', function(event) {
+            toggleOverlay(this, event);
+        });
     });
 });
 
 function toggleOverlay(card, event) {
     const overlay = card.querySelector('.card-overlay');
-    const clickCount = parseInt(card.dataset.clickCount);
-    const url = 'https://www.valencaride.com/src/cards/card-1/index.html';
+    const title = card.querySelector('.card-overlay h2');
 
-    if (clickCount === 0) {
-        // Mostrar el título en el primer clic
-        overlay.style.display = 'flex';
-        card.dataset.clickCount = 1;
-        event.preventDefault(); // Prevenir la redirección
-    } else if (clickCount === 1 && url) {
-        // Redirigir en el segundo clic si la URL está definida
-        window.location.href = url;
-     }
-        }
+    // Verificar si el título está visible
+    const titleVisible = title && window.getComputedStyle(title).getPropertyValue("opacity") === "1";
+
+    if (!titleVisible) {
+        // Mostrar el título
+        overlay.style.opacity = '1';
+    } else {
+        // Redirigir a otra página
+        const targetUrl = card.dataset.targetUrl; // Obtener la URL de la tarjeta
+        if (targetUrl) {
+            window.location.href = targetUrl; // Redirigir a la URL especificada
+        }
+    }
+
+    // Detener la propagación del evento para evitar que se active el evento click del contenedor padre
+    event.stopPropagation();
+}
