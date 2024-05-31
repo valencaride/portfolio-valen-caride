96125c33766eb814c9d514da6f898fc6d69f6f6e
arturogutierrez00 <arturo.g@bidcom.com.ar>
2024-05-31 15:54:34 -0300

    fix inicio v03

--- src/inicio/inicio.js
+++ src/inicio/inicio.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.card');
 
     cards.forEach(card => {
+        card.dataset.clickCount = 0; // Inicializa el contador de clics
+
         card.addEventListener('click', function(event) {
             toggleOverlay(this, event);
         });
@@ -10,22 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function toggleOverlay(card, event) {
     const overlay = card.querySelector('.card-overlay');
-    const title = card.querySelector('.card-overlay h2');
+    const clickCount = parseInt(card.dataset.clickCount);
 
-    // Verificar si el título está visible
-    const titleVisible = title && window.getComputedStyle(title).getPropertyValue("opacity") === "1";
-
-    if (!titleVisible) {
-        // Mostrar el título
-        overlay.style.opacity = '1';
-    } else {
-        // Redirigir a otra página
-        const targetUrl = card.dataset.targetUrl; // Obtener la URL de la tarjeta
-        if (targetUrl) {
-            window.location.href = targetUrl; // Redirigir a la URL especificada
-        }
+    if (clickCount === 0) {
+        // Mostrar el título en el primer clic
+        overlay.style.display = 'flex';
+        card.dataset.clickCount = 1;
+        event.preventDefault(); // Prevenir la redirección
+    } else if (clickCount === 1) {
+        // Redirigir en el segundo clic
+        window.location.href = card.dataset.url; //
     }
-
-    // Detener la propagación del evento para evitar que se active el evento click del contenedor padre
-    event.stopPropagation();
 }
